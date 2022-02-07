fd8fedd0d3f29f779a3cd2b6d0c641e063967d8c
Ihor Makieiev <makieiev@yahoo.com>
2022-02-07 21:08:21 +0200

    add db config

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@
 //    'pass' => 'cbece_gead-cebfa'
 //];
 return [
-    'dsn'  => 'mysql://b9f9c995031105:04a1118c@eu-cdbr-west-02.cleardb.net/heroku_5c27f63a8c0cef0?reconnect=true',
+    'dsn'  => 'mysql:host=eu-cdbr-west-02.cleardb.net:3306;dbname=heroku_5c27f63a8c0cef0;charset=utf8',
     'user' => 'b9f9c995031105',
     'pass' => '04a1118c'
 ];
