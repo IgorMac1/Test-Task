9a2b15890241ce1e11d4c0987ef44c416af09ae6
Ihor Makieiev <makieiev@yahoo.com>
2022-02-07 22:49:16 +0200

    make it better

--- config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'dsn'  => 'mysql:host=eu-cdbr-west-02.cleardb.net:3306;dbname=heroku_779f311677c565a;charset=utf8',
-    'user' => 'b5243b522a5e3f',
-    'pass' => '0cbfd2a2'
-];
