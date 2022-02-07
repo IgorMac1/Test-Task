d391667549d7e400dd709bf6453a037abce34e51
Ihor Makieiev <makieiev@yahoo.com>
2022-02-07 21:21:41 +0200

    add db config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,4 @@
 <?php
-//return [
-//    'dsn'  => 'mysql:host=a_level_mysql:3306;dbname=a_level_mysql;charset=utf8',
-//    'user' => 'root',
-//    'pass' => 'cbece_gead-cebfa'
-//];
 return [
     'dsn'  => 'mysql:host=eu-cdbr-west-02.cleardb.net:3306;dbname=heroku_5c27f63a8c0cef0;charset=utf8',
     'user' => 'b9f9c995031105',
